3b510b083463c9326d31e8df761eea3851115d97
Milos Knezevic <ushtipak@gmail.com>
2018-08-19 22:04:43 +0200

    Implement async task as part of the Stream :)

--- lib/plates_play/execute.ex
+++ lib/plates_play/execute.ex
@@ -12,22 +12,17 @@ defmodule Execute do
     File.stream!(wordlist_file)
     |> Stream.map(&String.trim/1)
     |> Stream.filter(fn(n) -> String.length(n) == 4 end)      
-    |> Stream.map(fn line -> async_word_check(line, plate_codes) end)
-    |> Stream.each(fn task -> await_and_inspect(task) end)
+    |> Stream.map(fn line ->
+         Task.async(fn ->
+           :poolboy.transaction(
+             :worker,
+             fn pid -> GenServer.call(pid, {line, plate_codes}) end,
+             @timeout
+           )
+         end)
+       end)
+    |> Stream.each(fn task -> task |> Task.await(@timeout) end)
     |> Stream.run
   end
 
-  defp async_word_check(word, plate_codes) do
-    Task.async(fn ->
-      :poolboy.transaction(
-        :worker,
-        fn pid -> GenServer.call(pid, {word, plate_codes}) end,
-        @timeout
-      )
-    end)
-  end
-
-  defp await_and_inspect(task), do:
-    task
-    |> Task.await(@timeout)
 end
